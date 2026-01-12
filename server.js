89f63945c4cd974f599b3ba30760d1a6751ede86
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2026-01-12 12:39:27 -0500

    Feat: Added Back-end configurations and Auth logic

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require("./src/app");
+const { connectDatabase } = require("./src/config/database");
+const { env } = require("./src/config/env");
+
+async function start() {
+	try {
+		app.listen(env.port, () => {
+			console.log(`API listening on port ${env.port}`);
+		});
+	} catch (err) {
+		console.error("Failed to start server", err);
+		process.exit(1);
+	}
+}
+
+start();
